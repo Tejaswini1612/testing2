08d4c1b317f744759a6a31976b39de059107e4c4
Sagar <Sagar@SAGAR>
2020-10-30 14:19:11 +0530

    by tej

---
+++ GitProject2/cjc/Demo.java
@@ -0,0 +1,21 @@
+//push on git page
+
+package com.cjc;
+
+public class Demo {
+	
+	public void sum()
+	{
+		System.out.println("gggggggggg");
+	}
+	public void sub()
+	{
+		System.out.println("sub");
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello...Main Method");
+	}
+
+}
---
+++ GitProject2/src/com/cjc/Demo.java
@@ -0,0 +1,17 @@
+//push on git page
+
+package com.cjc;
+
+public class Demo {
+	
+	public void sub()
+	{
+		System.out.println("sub");
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello...Main Method");
+	}
+
+}
